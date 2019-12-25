f6e53b137fbb54e9a1ae2ad03af0ec61e155c3c1
chengxufeng1994 <benny12628@gmail.com>
2019-12-25 23:22:50 +0800

    Updates

---
+++ precache-manifest.d2a1a7403b5d3fa6dc67a71b6915af7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf139b88ed0fe2dc99195b583affd1d",
+    "url": "/Marvel-api/index.html"
+  },
+  {
+    "revision": "1f8a8457747f36297cd1",
+    "url": "/Marvel-api/static/js/2.999c1e0a.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/Marvel-api/static/js/2.999c1e0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb47bd6cb2107291da50",
+    "url": "/Marvel-api/static/js/main.d089fa4b.chunk.js"
+  },
+  {
+    "revision": "d915ed26eeb3d605ab58",
+    "url": "/Marvel-api/static/js/runtime-main.0e31f803.js"
+  }
+]);
